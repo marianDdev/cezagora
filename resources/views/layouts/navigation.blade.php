ce9f2b9b95b0093bd0d9f8e27478bd2ec1393895
Marian Dumitru <d.mariandev@gmail.com>
2023-06-06 21:43:45 +0300

    UI - multiple dummy pages WIP

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -51,7 +51,7 @@
                 @if(Auth::check())
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
-                            <a href="#"
+                            <a href="{{ route('dashboard') }}"
                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
                         </li>
                         <li>
