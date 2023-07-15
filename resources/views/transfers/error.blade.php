6db71c78238031c483b5041eaa63de9929dddf1f
Marian Dumitru <d.mariandev@gmail.com>
2023-07-15 14:43:07 +0300

    - first collect the payment from customer - then execute the transfers to the sellers - notify selleers and customer abou the final payments

---
+++ resources/views/transfers/error.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+    <h1>The following error occured when tried to create the transfers for the sellers of order with id: #{{ $orderId }} </h1>
+    <p>{{ $error }}</p>
+</x-app-layout>
