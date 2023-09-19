45f6278f4aa10b23d6cffc75403bee430d3a78c3
Marian Dumitru <d.mariandev@gmail.com>
2023-09-19 08:56:20 +0300

    temp disabled non functional links

--- resources/views/pages/home/raw_materials.blade.php
+++ resources/views/pages/home/raw_materials.blade.php
@@ -5,7 +5,7 @@
         <img src="{{ url('/images/homepage/raw_materials.jpeg') }}">
     </div>
     <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Discover acids, alkalis, amino acids, siliocones, antioxidants, colours, emollients and more .</p>
-    <a href="#"
+    <a role="link" aria-disabled="true"
        class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Go to raw materials list
         <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
              viewBox="0 0 14 10">
