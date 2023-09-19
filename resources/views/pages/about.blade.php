bd8df7eededf2de12187bba34e42b9333d03551a
Marian Dumitru <d.mariandev@gmail.com>
2023-09-19 21:05:02 +0300

    stripe onboarding page revamped

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Supplies and services for cosmetics manufacturing</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Sell your products as a cosmetics manufacturer, sell cosmetics raw or packing materials as a supplier or offer your services as a laboratory or legal consultant.</p>
-                <a href="#"
+                <a href="{{ route('register') }}"
                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
@@ -14,9 +14,9 @@
                               clip-rule="evenodd"></path>
                     </svg>
                 </a>
-                <a href="#"
+                <a href="{{ route('contact') }}"
                    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Speak to Sales
+                    Contact us
                 </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
--- resources/views/stripe/onboarding.blade.php
+++ resources/views/stripe/onboarding.blade.php
@@ -1,10 +1,23 @@
 <x-guest-layout>
-    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-400">Please Connect with Stripe to set up your payments account before managing your products and services.</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Stripe Connect is the fastest and easiest way to integrate payments into CezAgora marketplace. Please complet this short onboarding steps to be able to receive payments for your products and services.</p>
-        <a href="{{ route('onboarding.redirect') }}"
-           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Clicke here to connect with stripe
-        </a>
-    </div>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-3xl dark:text-white">Please Connect with Stripe to set up your payments account before managing your products and services.</h1>
+                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Stripe Connect is the fastest and easiest way to integrate payments into CezAgora marketplace. Please complet this short onboarding steps to be able to receive payments for your products and services.</p>
+                <a href="{{ route('onboarding.redirect') }}"
+                   class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Signup on Stripe with no extra costs
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="{{ url('/images/stripe.png') }}">
+            </div>
+        </div>
+    </section>
 </x-guest-layout>
