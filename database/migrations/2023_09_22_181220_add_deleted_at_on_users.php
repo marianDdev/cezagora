78d9f13d2c5c2247aca65feeeb52cc12a230acfd
Marian Dumitru <d.mariandev@gmail.com>
2023-09-22 21:25:09 +0300

    CEZ-112-close-account - deleted_at and is_active on users

---
+++ database/migrations/2023_09_22_181220_add_deleted_at_on_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('email_verified_at', function (Blueprint $table) {
+                $table->timestamp('deleted_at')->nullable();
+                $table->boolean('is_active')->default(true);
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+            $table->dropColumn('is_active');
+        });
+    }
+};
