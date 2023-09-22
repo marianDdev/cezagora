3fecd14e15e2b178e4169b3eb0d0ff03569dd9c5
Marian Dumitru <d.mariandev@gmail.com>
2023-09-22 23:43:50 +0300

    CEZ-117-verify-is-active-at-login wip

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -26,6 +27,13 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
+
+        /** @var User $user */
+        $user = $request->user();
+
+        if ($user->is_active === false) {
+            return redirect('/activate-account');
+        }
 
         $request->session()->regenerate();
 
---
+++ resources/views/pages/activate_account.blade.php
@@ -0,0 +1,4 @@
+<x-guest-layout>
+<h1>It seems that you deleted your account. Would you like to reactivate it?</h1>
+    <!-- todo form here to update user and it's company with is_active = true and deleted_at = null-->
+</x-guest-layout>
