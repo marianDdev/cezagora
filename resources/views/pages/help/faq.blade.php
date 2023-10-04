f2135a2eca96d4c6f9862b06541bdc7a45fdd989
Marian Dumitru <d.mariandev@gmail.com>
2023-10-04 23:01:04 +0300

    CEZ-84-help-page - faq

--- resources/views/pages/help/faq.blade.php
+++ resources/views/pages/help/faq.blade.php
@@ -89,7 +89,7 @@
                             </svg>
                             How can I reset my password?
                         </h3>
-                        <p class="text-gray-500 dark:text-gray-400">You can reset your password by accessing our log-in page.</p>
+                        <p class="text-gray-500 dark:text-gray-400">You can reset your password by accessing the <a href="{{ route('password.request') }}" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">forgot password page</a>.</p>
                     </div>
                     <div class="mb-10">
                         <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
