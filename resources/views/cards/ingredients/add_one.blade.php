5f0d95e42d73ed435fa2a282810f335e8bc041d2
Marian Dumitru <d.mariandev@gmail.com>
2023-11-07 09:58:42 +0200

    CEZ-159-preselect-country, state and city

--- resources/views/cards/ingredients/add_one.blade.php
+++ resources/views/cards/ingredients/add_one.blade.php
@@ -24,7 +24,7 @@
                     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add ingredient</h3>
                     <form class="space-y-6" method="POST" action="{{ route('ingredient.store') }}">
                         @csrf
-                        <input type="hidden" name="company_id" value="{{ Auth::user()->company->id }}">
+                        <input type="hidden" name="company_id" value="{{ \Illuminate\Support\Facades\Auth::user()->company->id }}">
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">INCI name</label>
                             <input type="text" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="ex: Melaleuca alternifolia (Tea Tree) Leaf Oil">
@@ -55,21 +55,7 @@
                             <input type="number" name="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                             <x-input-error :messages="$errors->get('quantity')" class="mt-2" />
                         </div>
-                        <div>
-                            <label for="availability" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Availability method</label>
-                            <select name="availability"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            @foreach(\App\Services\Ingredient\IngredientServiceInterface::AVAILABILITY_TYPES as $availability)
-                                <option value="{{ $availability }}">{{ $availability }}</option>
-                                @endforeach
-                                </select>
-                            <x-input-error :messages="$errors->get('availability')" class="mt-2" />
-                        </div>
-                        <div>
-                            <label for="available_at" class="block mb-2 text-sm font-medium text-gray-900 ">Date when ingredients is available</label>
-                            <input type="date" name="available_at" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                            <x-input-error :messages="$errors->get('available_at')" class="mt-2" />
-                        </div>
+                        <livewire:show-available-at />
                         <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600">Create</button>
                     </form>
                 </div>
