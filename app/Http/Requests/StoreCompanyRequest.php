f40b46f9809031b27331fd44abb2940227442be8
Marian Dumitru <d.mariandev@gmail.com>
2023-11-13 22:47:45 +0200

    CEZ-164-fix-update-company - fixed modal closing button

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -19,19 +19,26 @@ class StoreCompanyRequest extends FormRequest
         //don't add Rule::exist() to company_category_id because we let the user add a new category
         // if none from the existing ones is his category
         return [
-            'company_categories' => ['required', 'array'],
+            'company_categories'   => ['required', 'array'],
             'company_categories.*' => ['required', 'int', Rule::exists(CompanyCategory::class, 'id')],
-            'name' => ['required', 'string', 'max:256'],
-            'email'      => ['required', 'email', 'unique:companies'],
-            'phone'      => ['required', 'regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10'],
-            'country' => ['required', 'string', 'max:256'],
-            'city' => ['required', 'string', 'max:256'],
-            'state' => ['nullable', 'string', 'max:256'],
-            'product_description' => ['nullable', 'string'],
-            'website' => ['nullable', 'url'],
-            'tax_id' => ['required', 'string', 'min:8', 'max:10'],
-            'vat_id' => ['required', 'string'],
-            'mcc' => ['required', 'string']
+            'name'                 => ['required', 'string', 'max:256'],
+            'email'                => ['required', 'email', 'unique:companies'],
+            'phone'                => ['required', 'regex:/^(?:\+|\b00)[1-9]\d{8,}$/'],
+            'country'              => ['required', 'string', 'max:256'],
+            'city'                 => ['required', 'string', 'max:256'],
+            'state'                => ['nullable', 'string', 'max:256'],
+            'product_description'  => ['nullable', 'string'],
+            'website'              => ['nullable', 'url'],
+            'tax_id'               => ['required', 'string', 'min:8', 'max:10'],
+            'vat_id'               => ['required', 'string'],
+            'mcc'                  => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'phone.regex' => 'Phone number format is invalid. Add country prefix without parentheses, dashes, or dots.'
         ];
     }
 }
