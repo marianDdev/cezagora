c3522a3c1099a58b19cf8a1ad537c7a2d4baff05
Marian Dumitru <d.mariandev@gmail.com>
2023-11-20 22:51:54 +0200

    CEZ-124-keep-modal-open-if-errors

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -1,18 +1,12 @@
 <x-guest-layout>
     <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 text-center">
-            @if(url()->current() === 'https://cezagora.test/my-ingredients')
-                <h1 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl">
+            <h1 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl">
 
-                    View your entire list of ingredients.
-                </h1>
-                <a class="mb-4 font-bold tracking-tight leading-none text-indigo-500"
-                   href="{{ route('ingredient.create') }}">Click here to add more ingredients</a>
-            @else
-                <h1 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white">
-                    Discover our entire list of ingredients.
-                </h1>
-            @endif
+                View your entire list of ingredients.
+            </h1>
+            <a class="mb-4 font-bold tracking-tight leading-none text-indigo-500"
+               href="{{ route('ingredient.create') }}">Click here to add more ingredients</a>
         </div>
     </section>
 
