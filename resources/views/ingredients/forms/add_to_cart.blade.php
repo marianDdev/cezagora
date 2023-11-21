04b5804ab621a72f2bcbc637bf306e644fb883b8
Marian Dumitru <d.mariandev@gmail.com>
2023-11-21 23:39:02 +0200

    CEZ-170-fix-go-to-materials - fixed

--- resources/views/ingredients/forms/add_to_cart.blade.php
+++ resources/views/ingredients/forms/add_to_cart.blade.php
@@ -20,8 +20,11 @@
     <select name="quantity"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option value="" selected>Choose quantity</option>
-        @foreach(range(1, $ingredient->quantity) as $quantity)
-            <option value="{{ $quantity }}">{{ $quantity }}</option>
+        @php
+            $quantity = $ingredient->quantity <= 99999 ? $ingredient->quantity : 99999;
+        @endphp
+        @foreach(range(1, $quantity) as $number)
+            <option value="{{ $number }}">{{ $number }}</option>
         @endforeach
     </select>
 
