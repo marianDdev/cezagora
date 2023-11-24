0b514c8da56cc695ddf9025cf3f38bfa3c6cce50
Marian Dumitru <d.mariandev@gmail.com>
2023-11-24 20:33:59 +0200

    CEZ-174-refactor-about-us-text

--- resources/views/payments/error.blade.php
+++ resources/views/payments/error.blade.php
@@ -2,7 +2,7 @@
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center">
-                <h1 class="mb-4 text-2xl tracking-tight font-extrabold lg:text-2xl text-primary-600 dark:text-primary-500">Whoops! The following error occured when tried to charge the customer for order with id: #{{ $orderId }}</h1>
+                <h1 class="mb-4 text-2xl tracking-tight font-extrabold lg:text-2xl text-primary-600 dark:text-primary-500">Whoops! Payment fail for order with id #{{ $orderId }}</h1>
                 <p class="mb-4 text-3xl tracking-tight font-bold text-red-500 md:text-4xl dark:text-white">{{ $error }}</p>
             </div>
         </div>
