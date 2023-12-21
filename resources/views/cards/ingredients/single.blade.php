aabafb52019c716e90fb68b07b45b20abbfc3f80
Marian Dumitru <d.mariandev@gmail.com>
2023-12-21 18:05:49 +0200

    CEZ-201-docs-info

--- resources/views/cards/ingredients/single.blade.php
+++ resources/views/cards/ingredients/single.blade.php
@@ -17,6 +17,9 @@
 
 <div
     class="{{ $ingredient->availability === \App\Services\Ingredient\IngredientServiceInterface::NOT_AVAILABLE ?  'bg-gray-100' : 'bg-blue-50'}} border border-blue-200 rounded-lg p-2 md:p-2">
+    @if($ingredient->documents->count() > 0)
+        @include('ingredients._documents_info', ['ingredient' => $ingredient])
+    @endif
     @if(!is_null($commonShortName))
         <p class="text-gray-500 dark:text-gray-400 mb-2">Common name: <span
                 class="font-bold text-gray-700">{{ $commonShortName }}</span></p>
@@ -41,20 +44,20 @@
         <a href="{{ route('ratings.index', ['slug' => $ingredient->company->slug]) }}" class="text-indigo-500">
             {{ $seller }}
 
-        @php
-            $averageRating = round($ingredient->company->receivedRatings()->avg('rating'));
-        @endphp
-        @if($ingredient->company->receivedRatings()->count() > 0)
-            <span class="flex items-center">
-                @for ($i = 1; $i <= 5; $i++)
-                    <svg class="w-4 h-4 {{ $i <= $averageRating ? 'text-yellow-300' : 'text-gray-300' }}"
-                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                @endfor
-            </span>
-        @endif
+            @php
+                $averageRating = round($ingredient->company->receivedRatings()->avg('rating'));
+            @endphp
+            @if($ingredient->company->receivedRatings()->count() > 0)
+                <span class="flex items-center">
+                    @for ($i = 1; $i <= 5; $i++)
+                        <svg class="w-4 h-4 {{ $i <= $averageRating ? 'text-yellow-300' : 'text-gray-300' }}"
+                             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                    @endfor
+                </span>
+            @endif
         </a>
     </p>
 
---
+++ resources/views/ingredients/_documents_info.blade.php
@@ -0,0 +1,44 @@
+<button type="button" data-modal-target="docs-info-modal" data-modal-toggle="docs-info-modal"
+        class="text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm p-2 inline-flex items-center justify-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+</button>
+
+<!-- Main modal -->
+<div id="docs-info-modal" tabindex="-1" aria-hidden="true"
+     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="p-4 md:p-5">
+                <p class="text-lg font-semibold">Delivery Documents</p>
+                <p class="text-gray-600 dark:text-gray-300">The ingredient will be delivered with the following documents included:</p>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5">
+                <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-toggle="docs-info-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <ul class="my-4 space-y-3">
+                    @foreach($ingredient->documents as $document)
+                    <li>
+                        <div
+                            class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                            <span class="flex-1 ms-3 whitespace-nowrap">{{ $document->name }}</span>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
