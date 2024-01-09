393d004f20122e2599b5a3b3b2136df4ef7f393e
Marian Dumitru <d.mariandev@gmail.com>
2024-01-09 22:57:20 +0200

    CEZ-195-signup-bonus - 3d secure payments (actions_needed)

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,34 +8,33 @@ use Illuminate\Http\Request;
 use Stripe\Exception\SignatureVerificationException;
 use Stripe\Stripe;
 use Stripe\Webhook;
+use UnexpectedValueException;
 
 class WebhookController extends Controller
 {
     public function handlePaymentIntent(Request $request)
     {
         Stripe::setApiKey(config('stripe.secret'));
-
-        $payload = $request->getContent();
+        $payload         = $request->getContent();
         $signatureHeader = $request->header('Stripe-Signature');
-        $event = null;
+        $event           = null;
 
         try {
             $event = Webhook::constructEvent(
-                $payload, $signatureHeader, config('stripe.payment_intent_webhook_secret')
+                $payload,
+                $signatureHeader,
+                config('stripe.payment_intent_webhook_secret')
             );
-        } catch(\UnexpectedValueException|SignatureVerificationException $e) {
-            return view('payments.error', ['error' => $e->getMessage(), 'orderId' => $order->id]);
+        } catch (UnexpectedValueException|SignatureVerificationException $e) {
+            return view('payments.error', ['error' => $e->getMessage()]);
         }
 
-        // Handle the checkout.session.completed event
         if ($event->type == 'payment_intent.succeeded') {
-            $paymentIntent = $event->data->object; // contains a StripePaymentIntent
-            // Handle the successful payment intent here
-            $order = Order::find($paymentIntent->metadata->order_id);
+            $paymentIntent = $event->data->object;
+            $order         = Order::find($paymentIntent->metadata->order_id);
             event(new OrderCreated($order));
 
             return view('payments.success', ['order' => $order]);
-            // Update order status or perform post-payment actions
         }
 
         return response()->json(['status' => 'success']);
