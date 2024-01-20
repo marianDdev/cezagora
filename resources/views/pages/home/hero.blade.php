3c91444e43545db6d3ef7f2a7101444ea4163b2f
Marian Dumitru <d.mariandev@gmail.com>
2024-01-20 14:21:13 +0200

    CEZ-221-refcator-flows-and-design - removed white space

--- resources/views/pages/home/hero.blade.php
+++ resources/views/pages/home/hero.blade.php
@@ -1,5 +1,5 @@
 <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="grid max-w-screen-xl px-4 mx-auto xl:gap-0 lg:grid-cols-12 pb-10">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{{ __('messages.discover_raw_materials_ingredients_services_in_one_place') }}</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"><a href="{{ route('register') }}" class="text-indigo-500 font-bold">{{ ucfirst(__('messages.signup')) }} {{ __('messages.now') }}</a>, {{ __('messages.join_manufacturers_suppliers_distributors_retailers') }}</p>
@@ -8,8 +8,8 @@
                 {{ __('messages.contact') }}
             </a>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="{{ url('/images/homepage/hero.png') }}">
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex rounded">
+            <img src="{{ url('/images/homepage/hero.png') }}" alt="hero image">
         </div>
     </div>
 </section>
