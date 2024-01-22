910b9cd5c783851d91a16425c8b3a222d728770a
Marian Dumitru <d.mariandev@gmail.com>
2024-01-22 09:08:32 +0200

    CEZ-221-refcator-flows-and-design - redesigned cart icon

--- resources/views/components/navigation-list.blade.php
+++ resources/views/components/navigation-list.blade.php
@@ -30,13 +30,13 @@
         </li>
         @if(Auth::check())
             <li>
-                @if(!is_null($authUser->company) && Order::where('customer_id', $authUser->company->id)
+                @if(!is_null($authUser->company) && is_null(Order::where('customer_id', $authUser->company->id)
                                  ->where('status', Order::STATUS_PENDING)
-                                 ->first() === null)
+                                 ->first()))
 
                     <!-- Modal toggle -->
                     <button data-modal-target="defaultModal" data-modal-toggle="defaultModal"
-                            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            class="block py-2 pl-3 pr-4 text-red-400 rounded hover:bg-red-500 md:hover:bg-transparent md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-red-500 dark:hover:text-white md:dark:hover:bg-transparent dark:border-red-500"
                             type="button">
                         <svg class="flex-1 w-8 h-8 fill-current" viewbox="0 0 24 24">
                             <path
@@ -57,7 +57,7 @@
                                         Your cart is empty.
                                     </h3>
                                     <button type="button"
-                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                             data-modal-hide="defaultModal">
                                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                              fill="none" viewBox="0 0 14 14">
