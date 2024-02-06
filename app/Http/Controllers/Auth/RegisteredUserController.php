0f12d962930986fbe8c87594ac616041ce8faf18
Marian Dumitru <d.mariandev@gmail.com>
2024-02-06 21:04:29 +0200

    CEZ-256-refactor-company-creation-role switcher in user menu

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use App\Services\Notification\NotificationServiceInterface;
 use App\Services\User\UserServiceInterface;
 use Illuminate\Auth\Events\Registered;
