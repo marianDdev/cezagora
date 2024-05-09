e2f0338dbfec85cb49cf7baca6572bb388167746
Marian Dumitru <d.mariandev@gmail.com>
2024-05-09 22:09:00 +0300

    CEZ-295-improve-search - using Algolia Scout Extended and upgraded to Laravel 11

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Searchable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Service extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, InteractsWithMedia, Searchable;
 
     protected $fillable = [
         'company_id',
@@ -24,5 +25,15 @@ class Service extends Model implements HasMedia
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function searchableAs(): string
+    {
+        return 'services_index';
+    }
+
+    public function toSearchableArray(): array
+    {
+        return $this->toArray();
     }
 }
--- app/Services/Search/SearchServiceInterface.php
+++ app/Services/Search/SearchServiceInterface.php
@@ -3,9 +3,12 @@
 namespace App\Services\Search;
 
 use App\Models\Company;
+use Illuminate\Support\Collection;
 
 interface SearchServiceInterface
 {
     public function globalSearch(string $keyword): array;
     public function create(string $keyword, ?Company $company): void;
+    public function searchCompanies(string $keyword): Collection;
+    public function searchIngredients(string $keyword): Collection;
 }
